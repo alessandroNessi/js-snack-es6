e12145349e098fc5f4bb4d0922531820b69d9b29
alessandroNessi <alessandronessi@msn.com>
2021-09-10 16:18:03 +0200

    correto bug dimensione max array

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,11 +1,13 @@
 const splitArray = (array, a, b) => {
+
+    // const newArray = array
+    
     const newArray=[]
     array.forEach((element,index) => {
         if(index>=a && index<=b){
             newArray.push(element);
         }
-    }
-    );
+    });
     return newArray;
 };
 document.getElementById("calculate").addEventListener("click", ()=>{
@@ -17,7 +19,8 @@ document.getElementById("calculate").addEventListener("click", ()=>{
     console.log(`array: ${array}`);
     console.log(`a:${a}`);
     console.log(`b:${b}`);
-    if(array.length>b+1 && a<b){
+    if(array.length>b && a<b){
+        console.log("chiamo la funzione");
         let result = splitArray(array,a,b);
         resultDiv.innerHTML=result;
     }
