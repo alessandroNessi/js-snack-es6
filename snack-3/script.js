b0a99d690b117cc1e025d56e6258a7d09a2ce985
alessandroNessi <alessandronessi@msn.com>
2021-09-10 16:08:10 +0200

    esercizio base completato

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,22 +1,24 @@
 const splitArray = (array, a, b) => {
-    if(a>=b){
-        return;
-    }
     const newArray=[]
-    array.forEach(element => {
-        newArray.push(element);
-        a++;
-        if(a==b){
-            return newArray;
+    array.forEach((element,index) => {
+        if(index>=a && index<=b){
+            newArray.push(element);
         }
-    });
+    }
+    );
+    return newArray;
 };
-document.getElementById()
-const result=document.getElementById("result");
-const array=document.getElementById("array").value;
-let ab = document.getElementById("ab").value.split(",");
-const a=parseInt(ab[0]);
-const b=parseInt(ab[1]);
-console.log(`array: ${array}`);
-console.log(`a:${a}`);
-console.log(`a:${b}`);
+document.getElementById("calculate").addEventListener("click", ()=>{
+    const resultDiv=document.getElementById("result");
+    const array=document.getElementById("array").value.split(",");
+    let ab = document.getElementById("ab").value.split(",");
+    const a=parseInt(ab[0]);
+    const b=parseInt(ab[1]);
+    console.log(`array: ${array}`);
+    console.log(`a:${a}`);
+    console.log(`b:${b}`);
+    if(array.length>b+1 && a<b){
+        let result = splitArray(array,a,b);
+        resultDiv.innerHTML=result;
+    }
+});
